269a0c0f05431b44b62ffce1e7b84ff04f23b096
Weslley S. Pereira <weslley.spereira@gmail.com>
2025-03-14 09:11:37 -0600

    Remove my email and CONDAENV from vlse_bench_run.sh

--- examples/vlse_benchmark/vlse_bench_run.sh
+++ examples/vlse_benchmark/vlse_bench_run.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-#SBATCH --account=aiuserapps
 #SBATCH --time=0:30:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=104
 #SBATCH --mem=32G
 #SBATCH --job-name=vlse-bench-run
-#SBATCH --mail-user=weslley.daSilvaPereira@nrel.gov
-#SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --output=vlse-bench-run.%j.out  # %j will be replaced with the job ID
 
 if command -v module &> /dev/null; then
     module load conda
-    conda activate /scratch/wdasilv/.conda-envs/py311-bbopt
+    conda activate $CONDAENV
 fi
 
 if [ $# -eq 0 ]; then
